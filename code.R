ec3e9ea6f89e515d80a4a513287a726242187abd
harrall <harrall@gmail.com>
2019-11-14 21:14:54 +0100

    bb graph

--- code.R
+++ code.R
@@ -1,1 +1,16 @@
 library(tidyverse)
+library(ggthemes)
+
+bb <- bb_19_13
+
+# Alt 1
+p <- bb %>%
+  mutate(region = reorder(region, hundrea, FUN = median)) %>%
+  ggplot() +
+  theme_economist() +
+  #  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  xlab("") + 
+  scale_y_continuous(labels = scales::percent) + 
+  geom_boxplot(aes(region, hundrea, fill = factor(year))) + 
+  labs(title = "100/10 Mbit/s dekning per region over tid")
+p
